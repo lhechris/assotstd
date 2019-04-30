9877b3c6aea0af1540114507f7c1c6abe98b924a
christophe Lhermitte <christophe.lhermitte@toulouse.viveris.com>
2019-04-30 15:30:49 +0200

    Create site

---
+++ site/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <!--<div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>-->
+<v-app id="assotstd">
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile to="/">
+          <v-list-tile-action>            
+              <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile to="/about">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+        <router-view/>
+        </v-layout>
+      </v-container>
+    </v-content>    
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+</v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
+<style>
+#assotstd {
+    background-image:url('/index_r6_c6.gif');
+    background-repeat:repeat;
+}
+
+
+</style>
---
+++ site/src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <!--<v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <div class="actu_up" ></div>
+      <div class="actu">
+      <p><iframe src="http://www.youtube.com/embed/Jieco-43WNI" frameborder="0" width="531" height="298"></iframe></p>
+      <p></p>
+      </div>
+      <div class="actu_down"></div>
+    </v-layout>
+  </v-container>-->
+  <div>
+    <div class="actu_up" ></div>
+    <div class="actu">
+    <p><iframe src="http://www.youtube.com/embed/Jieco-43WNI" frameborder="0" width="531" height="298"></iframe></p>
+    <p></p>
+    </div>
+    <div class="actu_down"></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+}
+</script>
+
+<style>
+.actu_up{
+  width: 720px;
+	height:216px;
+	padding-left:80px;
+	background-image:url(../assets/actu_up.png);
+  background-repeat:no-repeat;
+}
+.actu{
+  width: 660px;
+	margin-top:-15px;
+	padding-left:115px;
+	background-image:url(../assets/actu.png);
+  background-repeat:repeat;
+}
+.actu_down{
+    width: 720px;
+	height:216px;
+	margin-top:-15px;
+	padding-left:80px;
+	background-image:url(../assets/actu_down.png);
+    background-repeat:no-repeat;
+}
+
+</style>
